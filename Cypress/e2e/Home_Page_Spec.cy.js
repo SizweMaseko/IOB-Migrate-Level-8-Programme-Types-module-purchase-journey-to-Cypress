9ac99313db47b70926cabd4adc4267fa3f6ec10c
SizweMaseko <sizwe.maseko@platform45.com>
2024-03-27 09:38:20 +0200

    Add Level 8 Programme To Staging

---
+++ Cypress/e2e/Home_Page_Spec.cy.js
@@ -0,0 +1,13 @@
+it('Enrols in Financial Advice (QFA) program', () => {
+    // Open the website with credentials
+    cy.visit('https://learningneverends.iob.ie', {
+      auth: {
+        username: 'Adam',
+        password: 'IOB2020'
+      }
+    });
+  
+    // Set window size
+    cy.viewport(1334, 740); 
+  });
+  
